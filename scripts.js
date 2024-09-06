82d0acdf279ad60338c9a2f52df9892f76bda43f
Gabriel <gabrieldonatti97@gmail.com>
2024-09-06 10:28:42 -0300

    cep

--- scripts.js
+++ scripts.js
@@ -14,10 +14,10 @@ document.getElementById('cep').addEventListener('input', async function() {
             document.getElementById('resultado').innerHTML = '<p style="color: red;">CEP não encontrado.</p>';
         } else {
             document.getElementById('resultado').innerHTML = `
-                <p><strong>Endereço:</strong> ${data.logradouro}</p>
-                <p><strong>Bairro:</strong> ${data.bairro}</p>
-                <p><strong>Cidade:</strong> ${data.localidade}</p>
-                <p><strong>UF:</strong> ${data.uf}</p>
+                <p><label for="logradouro">Endereço:</label> <input type="text" id="logradouro" class="input-field" value="${data.logradouro}" readonly></p>
+                <p><label for="bairro">Bairro:</label> <input type="text" id="bairro" class="input-field" value="${data.bairro}" readonly></p>
+                <p><label for="cidade">Cidade:</label> <input type="text" id="cidade" class="input-field" value="${data.localidade}" readonly></p>
+                <p><label for="uf">UF:</label> <input type="text" id="uf" class="input-field" value="${data.uf}" readonly></p>
             `;
         }
     } catch (error) {
